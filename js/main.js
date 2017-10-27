3da3b0d66ffad45820d6ee704bfa37afcdf7efcf
Alin Chindea <alin.chindea@gmail.com>
2017-10-27 15:46:27 +0200

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
 //console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour)
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+//if any error occurs, check to see if you need quotations in brackets
+cardsInPlay.push(cardOne);
+console.log("User flipped queen");
+
+var cardTwo = cards[2]
+//if any error occurs, check to see if you need quotations in brackets
+cardsInPlay.push(cardTwo);
+console.log("User flipped king"); 
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!"); 
+	} else if (cardsInPlay[0] !== cardsInPlay[1]) {
+		alert("Sorry, try again");
+	}
+}
